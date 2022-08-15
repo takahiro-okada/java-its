63414de4668ade5cbd3a177f3580d454a3f68875
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-15 15:39:30 +0900

    課題作成のための入力フォームを作ろう

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -5,17 +5,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
     //    GET /issues
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    // Get /issues/creationForm
+    @GetMapping("/creationForm")
+    public String showCreationForm(){
+        return "issues/creationForm";
     }
 }
 

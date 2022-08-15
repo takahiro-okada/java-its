f5f6e29df6be2f2a9a5f23b5866c04d85f78bd08
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-15 15:14:39 +0900

    first commit

---
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -0,0 +1,22 @@
+package com.example.its.web.issue;
+
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class IssueController {
+
+    private final IssueService issueService;
+    //    GET /issues
+    @GetMapping("/issues")
+    public String showList(Model model){
+        model.addAttribute("issueList",IssueService.findAll());
+        return "issues/list";
+    }
+}
+
+

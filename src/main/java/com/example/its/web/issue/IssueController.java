00f75045a6f8506f024fb609a6a3454aad14f2d5
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-15 16:29:51 +0900

    2重サブミット対策をしよう

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -32,7 +32,7 @@ public class IssueController {
     @PostMapping
     public String create(IssueForm form,Model model){
         issueService.create(form.getSummary(),form.getDescription());
-        return showList(model); //TODO: リロードボタンの対策が必要
+        return "redirect:/issues"; //TODO: リロードボタンの対策が必要
     }
 }
 

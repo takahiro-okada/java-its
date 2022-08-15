1a8aa6fe3fd8105b14ede07d375e9d4da1e86a36
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-15 17:10:55 +0900

    課題詳細画面を実装しよう：Controller

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,15 +1,13 @@
 package com.example.its.web.issue;
 
+import com.example.its.domain.issue.IssueEntity;
 import com.example.its.domain.issue.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/issues")
@@ -38,6 +36,14 @@ public class IssueController {
         }
         issueService.create(form.getSummary(),form.getDescription());
         return "redirect:/issues";
+    }
+
+    //GET localhost:8080/issues/1
+    @GetMapping("/{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model) {
+        var dummyEntity = new IssueEntity(1, "概要", "説明");
+        model.addAttribute("issue", dummyEntity);
+        return "issues/detail";
     }
 }
 

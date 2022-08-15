e86a6709dc0cd38c194b8686cf13b3ba104bda01
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-15 16:02:33 +0900

    Viewからデータを受け取れるようにしよう

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,7 +24,7 @@ public class IssueController {
 
     // Get /issues/creationForm
     @GetMapping("/creationForm")
-    public String showCreationForm(){
+    public String showCreationForm(@ModelAttribute IssueForm form) {
         return "issues/creationForm";
     }
 

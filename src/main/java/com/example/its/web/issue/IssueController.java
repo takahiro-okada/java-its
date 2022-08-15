0231890a74beb137fc85e16493ee0350ecb508b1
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-15 16:51:39 +0900

    バリデーションを実装しよう

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -4,6 +4,8 @@ import com.example.its.domain.issue.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,9 +32,12 @@ public class IssueController {
 
     // POST /issues
     @PostMapping
-    public String create(IssueForm form,Model model){
+    public String create(@Validated IssueForm form, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            return showCreationForm(form);
+        }
         issueService.create(form.getSummary(),form.getDescription());
-        return "redirect:/issues"; //TODO: リロードボタンの対策が必要
+        return "redirect:/issues";
     }
 }
 
